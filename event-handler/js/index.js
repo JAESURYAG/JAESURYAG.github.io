42949d7dd7f8a7f9f714dace943cc3b422e8a9db
JAESURYAG <jaesuryaganesan96@gmail.com>
2021-04-07 04:08:16 +0800

    1938

--- event-handler/js/index.js
+++ event-handler/js/index.js
@@ -6,3 +6,16 @@ var elNamaPengguna = document.getElementById("namaPengguna"); // 1) select eleme
 elNamaPengguna.addEventListener("keyup", function(){
    elNamaPengguna.value = transformUppercase(elNamaPengguna.value);
 });
+
+function checkPassword(){
+  let elKataLaluan = document.getElementById("kataLaluan");
+  let checkPassword_val = elKataLaluan.value;  
+  if (checkPassword_val.length > 5){
+     alert("Pawword Check .... OK!");
+  } else {
+     alert("Password too short!");
+  }
+}
+
+let elKataLaluan = document.getElementById("kataLaluan");
+elKataLaluan.onblur = checkPassword;
