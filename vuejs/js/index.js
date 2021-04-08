4817068ea9148ddda5a82e3048da90d2ed143b28
JAESURYAG <jaesuryaganesan96@gmail.com>
2021-04-08 19:54:24 +0800

    2102

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -43,7 +43,7 @@ let app5 =  new Vue({
     data: {
         msg: "Jae Surya"
     },
-    method: {
+    methods: {
         reverseMsg: function (){
             this.msg = this.msg.split("").reverse().join("");
         }
